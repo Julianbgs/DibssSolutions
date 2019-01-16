b55a5a9a47059bfd406d91872864b16f0b56234e
Julian <julianbgs96@gmail.com>
2019-01-16 16:53:06 +0300

    adaptiv

--- public/js/script.js
+++ public/js/script.js
@@ -6,10 +6,55 @@ $(document).ready(function () {
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
-        arrows: true
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    arrows: false
+
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            }
+        ]
     });
 
     // close script
+
+    // mobile menu
+
+    $(document).on('click', '#header-trigger', function (event) {
+        event.preventDefault();
+        var menu = $('.header__menu');
+        if ($(this).hasClass('js-trigger-active')) {
+            $(this).removeClass('js-trigger-active');
+            menu.slideUp('slow');
+        } else {
+            $(this).addClass('js-trigger-active');
+            menu.slideDown('slow');
+        }
+        return false;
+    });
+
+    // close script
+
 
 
 });
